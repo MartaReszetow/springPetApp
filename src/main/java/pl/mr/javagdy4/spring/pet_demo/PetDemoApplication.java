aa36fab7df9ac57cbcead001e1ad1527f286ac27
MARTA RESZETOW <marta.reszetow@gmail.com>
2020-11-15 20:57:45 +0100

    initial commit

---
+++ src/main/java/pl/mr/javagdy4/spring/pet_demo/PetDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.mr.javagdy4.spring.pet_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mr/javagdy4/spring/pet_demo/PetDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mr.javagdy4.spring.pet_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
